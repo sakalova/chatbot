2e7b0f3b8deb0ec5441ca27e8cbda8cabfcc807c
maria <sakalovami@gmail.com>
2023-08-02 17:53:33 +0100

    refactor(gui): moved ChatBotWindow to a separate file

---
+++ gui.py
@@ -0,0 +1,29 @@
+from PyQt6.QtWidgets import (
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QTextEdit
+)
+
+
+class ChatBotWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setMinimumSize(700, 500)
+
+        # Widgets
+        # chat area widget
+        self.chat_area = QTextEdit(self)
+        self.chat_area.setGeometry(10, 10, 480, 320)
+        self.chat_area.setReadOnly(True)
+
+        # input field widget
+        self.input_area = QLineEdit(self)
+        self.input_area.setGeometry(10, 340, 480, 40)
+
+        # button widget
+        self.button = QPushButton("Send", self)
+        self.button.setGeometry(10, 380, 480, 40)
+
+        self.show()
--- main.py
+++ main.py
@@ -1,41 +1,10 @@
 import sys
 
-from PyQt6.QtWidgets import (
-    QApplication,
-    QLineEdit,
-    QMainWindow,
-    QPushButton,
-    QTextEdit
-)
+from PyQt6.QtWidgets import QApplication
+from gui import ChatBotWindow
 
 
-class ChatBotWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setMinimumSize(700, 500)
-
-        # Widgets
-        # chat area widget
-        self.chat_area = QTextEdit(self)
-        self.chat_area.setGeometry(10, 10, 480, 320)
-        self.chat_area.setReadOnly(True)
-
-        # input field widget
-        self.input_area = QLineEdit(self)
-        self.input_area.setGeometry(10, 340, 480, 40)
-
-        # button widget
-        self.button = QPushButton("Send", self)
-        self.button.setGeometry(10, 380, 480, 40)
-
-        self.show()
-
-
-class ChatBot:
-    ...
-
-
-app = QApplication(sys.argv)
-main_window = ChatBotWindow()
-sys.exit(app.exec())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = ChatBotWindow()
+    sys.exit(app.exec())
